9fec4ecc983e07a68ff6dde02120a02088c38815
npmFullStack <occ.mangorangca.norway@gmail.com>
2025-08-15 09:42:31 +0800

    updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json"
-  }
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+    timeout: 10000
 });
 
 // Add request interceptor to inject token
-api.interceptors.request.use((config) => {
-  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+api.interceptors.request.use(config => {
+    const token =
+        typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
